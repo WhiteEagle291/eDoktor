a753d0ca9062ec03efeda743e23abf92758c1786
WhiteEagle291 <djordjelazic01@gmail.com>
2025-03-08 18:06:58 +0100

    eDoktor commit

--- obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41805746932c39add9bb72e892e236e7d1e9dba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
